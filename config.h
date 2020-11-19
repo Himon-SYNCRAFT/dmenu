077913b310909e19ceee14f6c51214cc01627e84
Daniel Zawłocki <d.zawlocki@syncraft.pl>
2020-11-19 11:24:18 +0100

    config

--- config.h
+++ config.h
@@ -4,11 +4,11 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 static int colorprompt = 0;                /* -p  option; if 1, prompt uses SchemeSel, otherwise SchemeNorm */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 800;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-    "SauceCodePro Nerd Font-10",
+    "SauceCodePro Nerd Font-11",
 	"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
@@ -22,7 +22,7 @@ static const char *colors[SchemeLast][2] = {
 static unsigned int lines      = 10;
 static unsigned int columns    = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 0;
+static unsigned int lineheight = 11 * 2 + (11 / 2);
 static unsigned int min_lineheight = 8;
 
 /*
