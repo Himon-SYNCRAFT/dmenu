68e0a4a91e4c72ee0bfd3598ed24500cd1b4b79b
Daniel Zawlocki <d.zawlocki@syncraft.pl>
2023-09-05 08:01:40 +0200

    commit

--- config.h
+++ config.h
@@ -8,21 +8,31 @@ static int centered = 1;                    /* -c option; centers dmenu on scree
 static int min_width = 800;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-    "SauceCodePro Nerd Font-11",
 	"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#ebdbb2", "#312e39" },
-	[SchemeSel] = { "#ebdbb2", "#6262ae" },
-	[SchemeOut] = { "#ebdbb2", "#ff2975" },
+    /* catppuccin */
+	/* [SchemeNorm] = { "#CAD3F5", "#24273A" }, */
+	/* [SchemeSel] = { "#24273A", "#f0c6c6" }, */
+	/* [SchemeOut] = { "#CAD3F5", "#ED8796" }, */
+
+    /* TokyoNight */
+	[SchemeNorm] = { "#c0caf5", "#24283b" },
+	[SchemeSel] = { "#24283b", "#7aa2f7" },
+	[SchemeOut] = { "#c0caf5", "#f7768e" },
+
+    /* Kanagawa */
+	/* [SchemeNorm] = { "#dcd7ba", "#1f1f28" }, */
+	/* [SchemeSel] = { "#1f1f28", "#6a9589" }, */
+	/* [SchemeOut] = { "#dcd7ba", "#c34043" }, */
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 10;
 static unsigned int columns    = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 11 * 2 + (11 / 2);
+static unsigned int lineheight = 10 * 2 + (10 / 2);
 static unsigned int min_lineheight = 8;
 
 /*
